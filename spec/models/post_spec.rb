79e603abc82a85590de2df81c565388dff680b3a
cokeefe <chaemobile@gmail.com>
2013-09-18 14:59:49 -0700

    post new root parent fail

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,4 +1,17 @@
 require 'spec_helper'
 
 describe Post do
+  context "new root" do
+    let(:user) { User.create() }
+    let(:post) { Post.create(
+                    user: user.id,
+                    title: "First Title",
+                    content: "Testing Content" )}
+
+    it "should have a parent_id of nil" do
+    end
+
+    it "should have a root_id of nil" do
+    end
+  end
 end
