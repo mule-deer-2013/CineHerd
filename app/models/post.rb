dfb318c51effeb0768c67aa805dab23ed689943d
cokeefe <chaemobile@gmail.com>
2013-09-18 16:24:34 -0700

    uniqueness test passed

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ActiveRecord::Base
   belongs_to :user
   attr_accessible :content, :parent_id, :root_id, :title, :user_id
   validates_presence_of :content, :title, :user_id
+  validates_uniqueness_of :title
 end
