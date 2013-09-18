1d0611fb5cb857672be29a31f8b434ad074f49ad
cokeefe <chaemobile@gmail.com>
2013-09-18 16:14:58 -0700

    post model presence validations and tests

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   belongs_to :user
   attr_accessible :content, :parent_id, :root_id, :title, :user_id
-
+  validates_presence_of :content, :title, :user_id
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,13 +1,14 @@
 require 'spec_helper'
 
 describe Post do
-
   context "valid new root" do
+    let(:content) { "Testing Content" }
+    let(:title) { "First Title" }
     let(:user) { User.create() }
     let(:post) { Post.create(
                     user_id: user.id,
-                    title: "First Title",
-                    content: "Testing Content" )}
+                    title: title,
+                    content: content)}
 
     it "should have a parent_id of nil" do
       post.parent_id.should be nil
@@ -16,5 +17,30 @@ describe Post do
     it "should have a root_id of nil" do
       post.root_id.should be nil
     end
+
+    it "should have a title" do
+      post.title.should be title
+    end
+
+    it "should have content" do
+      post.content.should be content
+    end
   end
+
+ context "invalid new root" do
+
+   it "should raise error if attributes missing" do
+    post = Post.create()
+    post.errors.size.should
+   end
+
+   it "should not be saved if title is not unique" do
+     post = Post.create(:title => "First Title")
+     expect { second_post = Post.create(:title => "First Title")}.not_to change{Post.all.size}
+   end
+
+
+  end
+
+
 end
