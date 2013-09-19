6c0f6e69f42c6f34aade4ab34c3c911970976fda
cokeefe <chaemobile@gmail.com>
2013-09-18 17:48:29 -0700

    passing form tests

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,9 +4,17 @@ class PostsController < ActionController::Base
   end
 
   def new
+    @post = Post.new()
   end
 
   def create
+    @post = Post.new(params[:post])
+    @post.user_id = 1
+      if @post.save
+        redirect_to root_path
+      else
+        redirect_to new_post_path
+      end
   end
 
 end
