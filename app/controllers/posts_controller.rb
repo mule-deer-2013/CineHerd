22f3575cea4768d20302382d33ade00c9391129b
Dev Bootcamp <Apprentice@dbc33.local>
2013-09-19 15:17:23 -0700

    add descriptions for AR queries

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,6 @@
 class PostsController < ApplicationController
 
-include SessionsHelper
-
   def index
-    @user = current_user || User.new
     @questions = Post.where(parent_id: nil)
   end
 
@@ -22,8 +19,11 @@ include SessionsHelper
   end
 
   def show
+    # find the question for the page we're visiting
     @question = Post.find(params[:id])
+    # return an array of Post objects whose parent is the question, found by ID
     @answers = Post.where(:parent_id => params[:id]).where(:root_id => params[:id]).where(:parent_id => !nil)
+    # Return ALL answers for a question, regardless of Answer association. (They get parsed with a helper method in ApplicationContorller)
     @comments = Post.where(:root_id => params[:id]).where(:parent_id != :root_id)
   end
 
