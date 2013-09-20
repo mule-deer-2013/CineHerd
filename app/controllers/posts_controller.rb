c27e164a67f66806617727e8f9fc2bdc4ef8ef48
Dev Bootcamp <Apprentice@dbc33.local>
2013-09-20 10:26:17 -0700

    final commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,16 +11,33 @@ class PostsController < ApplicationController
   end
 
   def create
+
+    if params[:question]
+      q = params[:question]
+      @post = Post.new(
+        params[:post],
+        root_id: q,
+        parent_id: q,
+        user_id: current_user.id
+      )
+      @post.save
+      redirect_to root_path and return
+    end
+
     @post = Post.new(params[:post])
-    @post.user_id = 1
-      if @post.save
-        redirect_to root_path
-      else
-        redirect_to new_post_path
-      end
+    @post.user_id = current_user.id
+
+    if @post.save
+      redirect_to root_path
+    else
+      redirect_to new_post_path
+    end
+
   end
 
   def show
+    p params
+    p "X"*90
     # find the question for the page we're visiting
     @question = Post.find(params[:id])
     # return an array of Post objects whose parent is the question, found by ID
