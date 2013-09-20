d3d4680971ecd2fbbc1c9fbbb1f5e76cabe403fc
cokeefe <chaemobile@gmail.com>
2013-09-20 15:19:41 -0700

    comment model and controller outline

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130920220246) do
+ActiveRecord::Schema.define(:version => 20130920221548) do
+
+  create_table "comments", :force => true do |t|
+    t.integer  "user_id"
+    t.string   "title"
+    t.text     "content"
+    t.integer  "parent_id"
+    t.integer  "root_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "comments", ["user_id"], :name => "index_comments_on_user_id"
 
   create_table "posts", :force => true do |t|
     t.integer  "user_id"
