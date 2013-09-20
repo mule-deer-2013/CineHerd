18759e37e3caca3ed93972a2850209e65451fac7
cokeefe <chaemobile@gmail.com>
2013-09-20 15:55:09 -0700

    association of comment and user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
 
   attr_accessible :email, :username, :password
   has_many :posts
+  has_many :comments
   has_secure_password
 
   validates :username, presence: true
