506585e803c0454d450a3929a6e4b82fd55b0cb7
danapczynski <apczynski@gmail.com>
2013-09-20 15:46:03 -0700

    added cinemagraph fields to post model and view

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,41 @@
 class Post < ActiveRecord::Base
   belongs_to :user
-  attr_accessible :content, :parent_id, :root_id, :title, :user_id
+  attr_accessible :content, :parent_id, :root_id, :title, :user_id, :cinemagraph
   validates_presence_of :content, :title, :user_id
   validates_uniqueness_of :title
+
+  after_save :store_cinemagraph
+
+  def cinemagraph=(file_data)
+    unless file_data.blank?
+      @file_data = file_data
+      self.extension = file_data.original_filename.split('.').last.downcase
+    end
+  end
+
+  def cinemagraph_filename
+    File.join(CINEMAGRAPH_STORAGE, "#{self.id}.#{self.extension}")
+  end
+
+  def cinemagraph_path
+    "/cinemagraph_storage/#{self.id}.#{self.extension}"
+  end
+
+  def has_cinemagraph?
+    File.exists? cinemagraph_filename
+  end
+
+  CINEMAGRAPH_STORAGE = File.join(Rails.root, 'public', 'cinemagraph_storage')
+
+  private
+
+  def store_cinemagraph
+    if @file_data
+      FileUtils.mkdir_p CINEMAGRAPH_STORAGE
+      FILE.open(cinemagraph_filename, 'wb') do |f|
+        f.write(@file_data.read)
+      end
+      @file_data = nil
+    end
+  end
 end
