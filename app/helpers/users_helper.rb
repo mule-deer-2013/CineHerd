d3bc37aaa092f08f1887069393d889d7850b1e5e
danapczynski <apczynski@gmail.com>
2013-09-21 13:22:46 -0700

    added gravatars for users

---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,9 @@
+module UsersHelper
+  require 'digest'
+
+  def gravatar_for(user)
+    grav_id = Digest::MD5.hexdigest(user.email.downcase)
+    grav_link = "http://gravatar.com/avatar/#{grav_id}?s=200"
+  end
+
+end
