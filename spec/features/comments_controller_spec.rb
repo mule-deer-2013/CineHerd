ddb389e186e9e897b8f4b34ea5a74c13063cb049
cokeefe <chaemobile@gmail.com>
2013-09-22 09:42:09 -0700

    factory girl

--- spec/features/comments_controller_spec.rb
+++ spec/features/comments_controller_spec.rb
@@ -2,21 +2,16 @@ require 'spec_helper'
 
 feature 'Comments' do
   context "making a new comment" do
-    let(:content) { "Testing Content" }
-    let(:title) { "First Title" }
-    user = User.create(username: "hi", password: "123")
-    let(:post) { Post.create(
-                    user_id: user.id,
-                    title: title,
-                    content: content)}
+    let(:comment) { build(:comment) }
+    before(:each) { login(comment.user) }
 
     it "should have a link for a new comment" do
-      visit post_path(post.id)
+      visit post_path(comment.post)
       page.should have_content("Add a Comment")
     end
 
     it "should redirect to post_path if saved" do
-      visit new_post_comment(post)
+      visit new_post_comment_path(comment.post)
     end
   end
 end
