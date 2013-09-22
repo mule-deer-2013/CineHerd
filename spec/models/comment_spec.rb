112a368964a96ccb5b8cfce12194941104dfdf2e
cokeefe <chaemobile@gmail.com>
2013-09-22 14:01:06 -0700

    post and comment associations

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe Comment do
   it { should belong_to :user }
+  it { should belong_to :post }
   it { should validate_uniqueness_of :content }
   it { should validate_presence_of :content }
   it { should validate_presence_of :user }
