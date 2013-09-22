7d2166f821c487f0430235db436d5dccb2dc7979
cokeefe <chaemobile@gmail.com>
2013-09-22 14:07:55 -0700

    post has many comments:

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130922164642) do
+ActiveRecord::Schema.define(:version => 20130922210301) do
 
   create_table "comments", :force => true do |t|
     t.integer  "user_id"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(:version => 20130922164642) do
     t.integer  "root_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "post_id"
   end
 
   add_index "comments", ["user_id"], :name => "index_comments_on_user_id"
