0f2bfb942afea376de6d5e7d2f8bb55611fcf525
cokeefe <chaemobile@gmail.com>
2013-09-22 13:40:00 -0700

    create

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,5 +5,16 @@ class CommentsController < ApplicationController
   end
 
   def create
+    @comment = Comment.create(params[:comment])
+    @comment.user = current_user
+    @comment.parent_id = params[:post_id]
+    @comment.root_id = params[:post_id]
+
+    if @comment.save
+      redirect_to post_path(params[:post_id])
+    else
+      puts @comment.errors.full_messages
+      render "new"
+    end
   end
 end
