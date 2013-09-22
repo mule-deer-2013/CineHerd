0fc185bc26f2eb432799ca3420742838f064e908
cokeefe <chaemobile@gmail.com>
2013-09-21 23:48:20 -0700

    mass_assignment for comment

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -8,4 +8,6 @@ describe Comment do
   it { should validate_presence_of :user_id }
   it { should validate_presence_of :parent_id }
   it { should validate_presence_of :root_id }
+  it { should allow_mass_assignment_of :title }
+  it { should allow_mass_assignment_of :content }
 end
