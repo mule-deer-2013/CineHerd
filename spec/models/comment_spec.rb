b195cbe1798bdd9de96f4657d95f601338df6ef5
cokeefe <chaemobile@gmail.com>
2013-09-22 09:42:46 -0700

    user_id to user to ensure database record exists

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -5,7 +5,7 @@ describe Comment do
   it { should validate_uniqueness_of :title }
   it { should validate_presence_of :title }
   it { should validate_presence_of :content }
-  it { should validate_presence_of :user_id }
+  it { should validate_presence_of :user }
   it { should validate_presence_of :parent_id }
   it { should validate_presence_of :root_id }
   it { should allow_mass_assignment_of :title }
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Post do
   it { should validate_presence_of :content }
   it { should validate_presence_of :title }
-  it { should validate_presence_of :user_id }
+  it { should validate_presence_of :user }
   it { should validate_presence_of :extension }
 
   it { should allow_mass_assignment_of :cinemagraph}
