0e84dbf5bc2e2f05cf934c75821aafb5bf2a4a0b
cokeefe <chaemobile@gmail.com>
2013-09-21 23:46:16 -0700

    factory

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,15 +1,17 @@
 require 'spec_helper'
 
 describe Post do
+  it { should validate_presence_of :content }
+  it { should validate_presence_of :title }
+  it { should validate_presence_of :user_id }
+  it { should validate_presence_of :extension }
+  it { should allow_mass_assignment_of :cinemagraph}
+  it { should validate_uniqueness_of :title}
+
+  it { should belong_to :user }
+
   context "valid new root" do
-    let(:content) { "Testing Content" }
-    let(:title) { "First Title" }
-    let(:user) { User.create }
-    let(:post) { Post.create(
-                    user_id: user.id,
-                    title: title,
-                    content: content,
-                    extension: 'gif')}
+    let(:post) { build(:post) }
 
     it "should have a parent_id of nil" do
       post.parent_id.should be_nil
@@ -18,25 +20,5 @@ describe Post do
     it "should have a root_id of nil" do
       post.root_id.should be_nil
     end
-
-    it "should have a title" do
-      post.title.should be title
-    end
-
-    it "should have content" do
-      post.content.should be content
-    end
-  end
-
- context "invalid new root" do
-   it "should raise error if attributes missing" do
-    post = Post.create()
-    post.errors.size.should > 0
-   end
-
-   it "should not be saved if title is not unique" do
-     post = Post.create(:user_id => 1, :title => "First Title", :content => "Bananas")
-     expect { second_post = Post.create(:user_id => 2, :title => "First Title", :content => "Cookie")}.not_to change{Post.all.size}
-   end
   end
 end
