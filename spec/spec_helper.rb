3c10aa4e4a3e2d0e3750ee97f194b708e63c5989
cokeefe <chaemobile@gmail.com>
2013-09-22 01:47:59 -0700

    database cleaner

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,17 @@ RSpec.configure do |config|
   config.include AuthenticationHelper
   config.include FactoryGirl::Syntax::Methods
 
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 
   # ## Mock Framework
   #
@@ -30,7 +41,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   # If true, the base class of anonymous controllers will be inferred
   # automatically. This will be the default behavior in future versions of
