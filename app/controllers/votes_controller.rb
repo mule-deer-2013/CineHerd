23c898b0a0f71e697ae71adf0f20379f795ef6a1
danapczynski <apczynski@gmail.com>
2013-09-23 01:25:45 -0700

    tweaked votes controller for db validations

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,20 +1,24 @@
 class VotesController < ApplicationController
 
   def createup
-    @vote = Vote.create(upvote_or_downvote: true, votable_id: params[:post], votable_type: "post", user_id: current_user.id )
-    flash[:error] = @vote.errors.full_messages unless @vote.save
-
+    @vote = Vote.new(upvote_or_downvote: true, votable_id: params[:post], votable_type: "post", user_id: current_user.id )
+    begin
+      flash[:error] = @vote.errors.full_messages unless @vote.save
+    rescue
+    end
     if params[:location] == 'index'
-      redirect_to post_path(params[:post])
+        redirect_to post_path(params[:post])
     else
       redirect_to root_path
     end
   end
 
   def createdown
-    @vote = Vote.create(upvote_or_downvote: false, votable_id: params[:post], votable_type: "post", user_id: current_user.id )
-    flash[:error] = @vote.errors.full_messages unless @vote.save
-
+    @vote = Vote.new(upvote_or_downvote: false, votable_id: params[:post], votable_type: "post", user_id: current_user.id )
+    begin
+      flash[:error] = @vote.errors.full_messages unless @vote.save
+    rescue
+    end
     if params[:location] == 'index'
       redirect_to post_path(params[:post])
     else
