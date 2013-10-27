c36af88a44a9b09b59936fdd8378700fdaae2783
cokeefe <chaemobile@gmail.com>
2013-10-27 16:01:50 -0400

    disable arrows if user not signed in

--- app/assets/javascripts/post.js
+++ app/assets/javascripts/post.js
@@ -1,12 +1,34 @@
-$(document).ready(function(){
+
+var SubmitPost = function() {
+}
+
+SubmitPost.prototype.afterSubmit = function() {
   $("#new_post_form").on('ajax:success', function(xhr, settings){
     displayLoadingAnimation()
   }).on('ajax:complete', function(e, data, status, xhr){
     console.log(data)
   })
-})
-
-function displayLoadingAnimation() {
-  $("#new_post_button").removeClass('btn-primary').addClass('btn-danger')
-  $("#new_post_button").val('Uploading Cinemagraph...')
 }
+
+var Arrows = function(images){
+  this.arrowImages = images
+  this.dissableButton = $('#sign_in')
+}
+
+Arrows.prototype.disable = function(){
+  if (this.dissableButton.length > 0)
+  {
+    $.each(this.arrowImages, function(index, value){
+      $(value).attr('onClick', 'return false')
+    })
+  }
+}
+
+
+$(document).ready(function(){
+  newPost = new SubmitPost()
+  newPost.afterSubmit()
+
+  arrowImages = new Arrows($('i'))
+  arrowImages.disable()
+})
